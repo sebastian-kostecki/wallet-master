8a448414d7a044858830e923cdd9da7320659696
Sebastian Kostecki <sebastian@sebastian.localdomain>
2026-05-28 16:11:11 +0200

    feat(account): add method to retrieve accounts for a user

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use App\Enums\AccountType;
 use App\Enums\Bank;
 use Database\Factories\AccountFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -32,6 +33,15 @@ final class Account extends Model
     use HasFactory;
 
     use SoftDeletes;
+
+    public static function getForUser(User $user): Collection
+    {
+        return self::query()
+            ->whereBelongsTo($user)
+            ->orderBy('name')
+            ->with('currency')
+            ->get();
+    }
 
     protected $guarded = [];
 
