30c2b4fbd1ee96214a3e79918951389208ab007d
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2026-06-09 08:30:21 +0200

    chore(app): enable SecurityHeaders middleware in web stack

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,7 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->web(prepend: [
-            // SecurityHeaders::class,
+            SecurityHeaders::class,
             SetApplicationLocale::class,
         ]);
 
